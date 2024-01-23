9156f78a9c89ea1e35e6b65dc3d9c4db8eeca316
ReshabSingh <145447207+ReshabSingh@users.noreply.github.com>
2024-01-23 18:26:20 +0000

    datatypes in javascript

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,47 @@
+// Primitive and NonPrimitive
+
+// Primitive --> String , Number , Boolean, null , undefined , BigInt,Symbol
+
+// Non-Primitive (Reference)
+// Arrays , objects and functions
+
+
+// console.log(typeof "aha")
+// console.log(typeof 12345)
+
+
+/*
+typeof --> datatype of the variable 
+
+*/
+// let isopen =1;
+// console.log(typeof isopen);
+// isopen= Boolean(isopen)
+// console.log(typeof isopen);
+
+// conversion of boolean value from number 
+// 1--> true 0-->  false 
+
+// console.log((3 + 5)* 8 %8)
+
+let age=20
+let name ='Vishal'
+let bank_balance=222222222
+let isrich=true
+let g=null
+
+// console.log(typeof age);
+// console.log(typeof name);
+// console.log(typeof bank_balance);
+// console.log(typeof isrich);
+// console.log(typeof g);
+
+let myObj ={
+    name :"reshab",
+    work:"software_engineer",
+    age:39,
+};
+
+// console.log(myObj.name);
+// console.log(typeof myObj);
+
