aa49c420c6c0f5c1c395907289b4de0643122e63
ReshabSingh <145447207+ReshabSingh@users.noreply.github.com>
2024-06-14 18:40:35 +0000

    Loops HOF

---
+++ 04_iterators/seven.js
@@ -0,0 +1,19 @@
+// // const mynums =[1,2,3,4,5,6,7,8,9,10,11,21,47,97,53,13,1,19]
+
+// // const newNums = mynums.map( (num)=> num + 10)
+// // console.log(newNums);
+
+// // let newNums2 = mynums.forEach((num)=> num+ 10)
+// // console.log(newNums2)
+
+// // let newNums = mynums.map( (num)=> num % num ==0 && num % 1 ==0 )
+// // console.log(newNums);
+
+// const mynums = [1,2,3,4,5]
+
+// const mytotal = mynums.reduce((acc,currval)=> {
+//     console.log(`acc:${acc} and currval is: ${currval}`);
+//     return acc + currval
+// } )
+
+// console.log(mytotal);
