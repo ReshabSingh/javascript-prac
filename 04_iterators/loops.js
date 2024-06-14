31179fe3b8857dcd1c75e8cb99e3fccd868932d3
ReshabSingh <145447207+ReshabSingh@users.noreply.github.com>
2024-06-14 10:30:23 +0000

    loops

---
+++ 04_iterators/loops.js
@@ -0,0 +1,28 @@
+// for (const iterator of object) {
+    
+// }
+
+const myarr = [1,2,3,4,5,6]
+
+// for (const val of myarr) {
+//     console.log(`Value is ${val}`)
+// }
+
+// array.forEach(element => {
+    
+// });
+// myarr.forEach( function (val){
+
+//     console.log(val);
+// } )
+
+// myarr.forEach( (item) =>{
+//     console.log(item);
+// } )
+
+// function Printme(params) {
+//     console.log(params);
+// }
+
+// myarr.forEach(Printme)
+
